a04557756e7cfaa588042736ea83572429703848
victor FernandezF <victorfernandezflores2@gmail.com>
2022-10-03 23:26:24 +0200

    Styles Changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ function alertMessage($message){
 }
 
 if (isset($_POST['maketor'])){
-    $number = $_POST['numero'];
+    $number = $_POST['number'];
     $area = $_POST['participantes'];
     $participants = explode("\r",$area);
     $check = checkCorrectNumberOfParticipants($number, $participants);
@@ -29,7 +29,7 @@ if (isset($_POST['maketor'])){
             $rand2 = array_rand($participants, 1);
             $participant2 = $participants[$rand2];
             unset($participants[$rand2]);
-            $newArray[] = "$participant1 VS $participant2";            
+            $newArray[] = "$participant1 <span class='vs'>VS</span> $participant2";            
         }
     $_SESSION['saveArray'] = $newArray;
     }
@@ -44,7 +44,7 @@ if (isset($_POST['maketor'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/styles_tournament.css">
     <link rel="stylesheet" href="styles/styles_tournament_phone.css">
-    <title>Document</title>
+    <title>Tournament Maker</title>
 </head>
 <body>
     <div class="mega-container">
@@ -60,15 +60,16 @@ if (isset($_POST['maketor'])){
                         <form action="" method="POST">
                             <fieldset>
                                 <label>Nº DE PARTICIPANTES:<span>*</span></label><br>
-                                <select class="select" name="numero">
+                                <select class="select" id="number" name="number">
                                     <option value="4">4 PARTICIPANTES</option>    
                                     <option value="8" selected>8 PARTICIPANTES</option>    
                                     <option value="16">16 PARTICIPANTES</option>    
                                 </select>
                             </fieldset>
                             <fieldset>
-                                <label>PARTICIPANTES:<span>*</span></label><br>
-                                <textarea class="textarea" type="textarea" cols ="10" row="10" id="participantes" name="participantes"></textarea>
+                                <label>PARTICIPANTES:<span>*</span></label>
+                                <label class="count" id="count">Participantes: 0</label><br>
+                                <textarea class="textarea" type="textarea" cols ="10" row="10" id="participants" oninput="count()" name="participantes"></textarea>
                             </fieldset>
                             <fieldset>
                                 <div class="<?= isset($_SESSION['saveArray'])? "result" : "hide" ?>">
@@ -94,3 +95,31 @@ if (isset($_POST['maketor'])){
                         </form>
                     </div>
                 </div>
+            <script>
+                
+                function count(){
+                    let textarea = document.getElementById('participants');
+                    let text = textarea.value;
+                    let e = document.getElementById("number");
+                    let select = e.options[e.selectedIndex].value;
+                    let count = document.getElementById('count');
+                    words =  text.split(/\r\n|\r|\n/).length;
+                    document.getElementById('count').innerHTML = 'Participantes: ' + words;
+                    if (words > select) {
+                        count.style.color="red";
+                        count.style.fontWeight="bold";
+                        document.getElementById('count').innerHTML = 'DEMASIADOS';
+                    }
+                    else if (words == select){
+                        count.style.color="green";
+                        count.style.fontWeight="bold";
+                        document.getElementById('count').innerHTML = 'CORRECTO';
+
+                    }else{
+                        count.style.color="black";
+                        count.style.fontWeight="normal"; 
+                    };
+                   
+                }
+
+            </script>
